22321860654d6a1b81d5b079dc5f084f5167f911
winarwahyuw <winarwahyuw99@gmail.com>
2022-06-10 14:18:19 +0700

    test passed

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require('fs');
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +19,42 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+async function bacaData(fnCallback){
+  const files = [file1, file2, file3];
+  const resultArray = [];
+
+  try{
+    for(let i=0; i<files.length; i++){
+      let promise = new Promise((resolve)=>{
+        fs.readFile(files[i], (err, data)=>{
+          const dataObjects = JSON.parse(data);
+          resolve(getData(dataObjects));
+        });
+      }) ;
+      let results = await promise;
+      resultArray.push(results);
+    }
+    
+    return fnCallback(null, resultArray);
+  }catch (err){
+    return fnCallback(err, resultArray);
+  }
+};
+
+function getData(dataObjects){
+  if(dataObjects.message !== undefined){
+    return splitWords(dataObjects.message);
+  }else if(dataObjects[0].message !==undefined){
+    return splitWords(dataObjects[0].message);
+  }else{
+    return splitWords(dataObjects[0].data.message);
+  }
+}
+
+function splitWords(words){
+  return words.split(' ')[1];
+}
+
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
